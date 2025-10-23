bb4d65bf41ea74a61c69aa3c18e331e9e232982d
Jitender <jitenderyadav330@gmail.com>
2025-10-23 23:43:02 +0530

    feat: set up Express server with MongoDB connection and error handling
    - Updated package.json to include new dependencies: cookie-parser, cors, express, mongoose, and prettier.
    - Created app.js to initialize Express app with middleware for CORS, JSON parsing, URL encoding, and cookie parsing.
    - Added constants.js for database name configuration.
    - Implemented index.js to configure environment variables and start the server after connecting to MongoDB.
    - Developed db/index.js for MongoDB connection logic with error handling.
    - Introduced utils/anyncHandler.js for handling asynchronous route handlers.
    - Created utils/apiError.js for structured error responses.
    - Added utils/apiResponse.js for standardized API responses.
    - Initialized .vscode/settings.json for project settings.

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import { app } from "../app.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (error)=>{
+      console.log(`err:`, error);
+      throw error
+    })
+    console.log(`n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB Connection Failed (index (DB) file)", error);
+    process.exit(1);
+  }
+};
+
+
+export default connectDB;
